5a4d1821abeca2e3e156a28c5df0b43745f9391e
ThomasZago <thom.zago@gmail.com>
2023-06-20 13:35:56 +0200

    feat : Creation of the first DAO Class for the Login HttpClientUserDAO.cs (with fictive url) feat : Creation ConnectedUser.cs to keep information of the connected user

---
+++ MelissandreDepartment/DAO/HttpClientUserDAO.cs
@@ -0,0 +1,70 @@
+﻿using MelissandreDepartment.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MelissandreDepartment.DAO
+{
+    public class HttpClientUserDAO
+    {
+        private readonly String apiUrl = "https://temporaryID/api/service";
+        private readonly HttpClient _httpClient;
+        private static HttpClientUserDAO _instance;
+        private static readonly object _lockObject = new object();
+
+        public static HttpClientUserDAO Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lockObject)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new HttpClientUserDAO();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        private HttpClientUserDAO()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<(bool success, string role, string token, string message)> Login(string email, string password)
+        {
+            String requestUrl = $"{apiUrl}/login?email={email}&password={password}";
+
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(requestUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    String role = response.Headers.GetValues("Role").First();
+                    String token = response.Headers.GetValues("Token").First();
+                    return (true, role, token, null);
+                }
+                else
+                {
+                    String message = response.Headers.GetValues("message").FirstOrDefault("There was an error in the request. \nPlease retry later");
+                    return (false, null, null, message);
+                }
+            }
+            catch (HttpRequestException exception)
+            {
+                String message = $"There was an error:\n{exception.Message}\nPlease, send this to your administrator.";
+                return (false, null, null, message);
+            }
+        }
+    }
+}
---
+++ MelissandreDepartment/Model/ConnectedUser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MelissandreDepartment.Model
+{
+    public class ConnectedUser
+    {
+        private static ConnectedUser _instance;
+        private static readonly object _lockObject = new object();
+
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public String Role { get; set; }
+        public String SessionToken { get; set; }
+
+        private ConnectedUser()
+        {
+
+        }
+
+        public static ConnectedUser Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lockObject)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new ConnectedUser();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
