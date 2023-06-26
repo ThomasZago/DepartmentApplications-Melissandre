8557cfd538d1772dc5204fc2f6c0405686cbae8f
ThomasZago <thom.zago@gmail.com>
2023-06-26 21:16:24 +0200

    feat : Style for ServiceAccountManagementView.xaml. Updated dataGridStyle on StylesDictionary.xaml

---
+++ MelissandreServiceLibrary/Converter/EnumValueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MelissandreServiceLibrary.Converter
+{
+    public class EnumValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is System.Enum enumValue)
+            {
+                return enumValue.ToString();
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
