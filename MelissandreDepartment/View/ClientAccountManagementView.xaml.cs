993138d575baf543a592edb12efc0b3c3ed4ad5c
ThomasZago <thom.zago@gmail.com>
2023-06-27 14:32:39 +0200

    feat : Creation of ClientAccountManagementView.xaml

---
+++ MelissandreDepartment/View/ClientAccountManagementView.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MelissandreDepartment.View
+{
+    /// <summary>
+    /// Logique d'interaction pour ClientAccountManagementView.xaml
+    /// </summary>
+    public partial class ClientAccountManagementView : Page
+    {
+        private static ClientAccountManagementView _instance;
+        private static readonly object _lockObject = new object();
+
+        private ClientAccountManagementView()
+        {
+            InitializeComponent();
+        }
+
+        public static ClientAccountManagementView Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lockObject)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new ClientAccountManagementView();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
